5762af310b4061123a3e2ced0c353b2e35a46123
hachazo <87842940+hachazo@users.noreply.github.com>
2023-10-16 03:57:37 -0300

    up

--- database/database.py
+++ database/database.py
@@ -26,15 +26,13 @@ class PersonaDb(Conection):
         return row
         
     def actualizar(self,row):
-        self._cursor.execute("SELECT * FROM usuarios WHERE id_usuario = %s", (row[0],))
+        self._cursor.execute("SELECT * FROM usuarios WHERE id_usuario = %s",(row[0],))
         if self._cursor.fetchone() is not None:
             self._cursor.execute("UPDATE usuarios SET usuario = %s, clave = %s,nombre = %s, apellido = %s, dni = %s, correo_electronico = %s, nro_telefono = %s, id_rol = %s, estado = %s, tipo = %s, dir_calle = %s, dir_ciudad = %s, dir_provincia = %s, dir_codigopostal = %s, dir_pais = %s", (row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11],row[12],row[13],row[14],row[15]))
             self._connection.commit()
             return True
-            print("true")
         else:
             return False
-            print("false")
         
     def borrado(self,usuario):
         self._cursor.execute("DELETE FROM usuarios WHERE usuario= '{usuario}'")
