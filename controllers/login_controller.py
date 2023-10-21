55026b826e3792e9969fef935ba731b8239eb94f
hachazo <87842940+hachazo@users.noreply.github.com>
2023-10-21 15:10:40 -0300

    up

--- controllers/login_controller.py
+++ controllers/login_controller.py
@@ -1,67 +1,82 @@
+from typing import Any
 from  PyQt5 import QtWidgets, uic
 import time
 from database.database import PersonaDb
+
 # Iniciar la aplicación
 app = QtWidgets.QApplication([])
 login = uic.loadUi('views/login.ui')
 main = uic.loadUi('views/main.ui')
 db = PersonaDb()
+        
+class LoginController(): # Logica de negocio 
+    
+    def __init__(self, login_ui):
+        self.login_ui = login
+        self.db = PersonaDb()
+    
+    def gui_login(self):
+        try:
+            name = self.login_ui.user.text()
+            password = self.login_ui.password.text()
+            row = self.db.leer(name,password)
+            
+            if len(name)==0 or len(password)==0:
+                raise Exception("Por favor, ingrese un usuario y contraseña")
+            
+            if row is None:
+                raise Exception("Usuario o contraseña incorrectos")
+            
+            self.login_ui.error.setStyleSheet("color: green")
+            self.login_ui.error.setText("Iniciando sesión...")
+                    
+            if str(row[1]) == str("admin") and row[2] == str("admin"):
+                LoginController.show_page_2(list(row))
+            
+            Main.show_page_3(list(row))
+            self.login_ui.close()
+        
+        except Exception as e:
+            self.login_ui.error.setText(str(e))
+            
+    def show_page_2(self,row):
+        try:
+            name = self.login_ui.user1.text()
+            password = self.login_ui.password1.text()
+        
+            if len(name)==0 or len(password)==0:
+                raise Exception("Por favor, ingrese un usuario y contraseña")
+        
+            if name =="admin" and password =="admin":
+                raise Exception("Ingrese un usuario y contraseña distintos")
+            
+            row[1] = name
+            row[2] = password
+            if self.db.actualizar(row) is False:
+                raise Exception("Error, revise tu conexión a internet")
+                
+            self.login_ui.error_1.setStyleSheet("color: green")
+            self.login_ui.error_1.setText("Contraseña cambiada con éxito")
+            time.sleep(1)
+            self.login_ui.close()
+            Main.show_page_3(row)
+        
+        except Exception as e:
+            self.login_ui.error_1.setText(str(e))
 
-# Cargar el archivo .ui
 class Login():
     
-    def gui_login():
-        name = login.user.text()
-        password = login.password.text()
-        row = db.leer(name,password)
-        
-        if len(name)==0 or len(password)==0:
-            login.error.setText("Por favor, ingrese un usuario y contraseña")  
-        else:
-            if row is None:
-                login.error.setText("Usuario o contraseña incorrectos")
-            else:
-                login.error.setStyleSheet("color: green")
-                login.error.setText("Iniciando sesión...")
-                if str(row[1]) == str("admin") and row[2] == str("admin"):
-                    login.stackedWidget.setCurrentIndex(1)
-                    login.log_in_1.clicked.connect(lambda: Login.show_page_2(list(row))) 
-        
-                time.sleep(1)
-                #login.close()
-                #app.quit()
-                #login.log_in.clicked.connect(main.show())
-        # login.stackedWidget.setCurrentIndex(2)
-        # login.log_in_2.clicked.connect(lambda: Login.show_page_3(row))
-        
-    def show_page_2(row):
-        name = login.user1.text()
-        password = login.password1.text()
-        
-        if len(name)==0 or len(password)==0:
-            login.error_1.setText("Por favor, ingrese un usuario y contraseña")
-        else:
-            if name =="admin" and password =="admin":
-                login.error_1.setText("Ingrese un usuario y contraseña distintos")
-            else:
-                row[1] = name
-                row[2] = password
-                if db.actualizar(row) is False:
-                 login.error_1.setText("Error, revise tu conexión a internet")
-                else:
-                    login.error_1.setStyleSheet("color: green")
-                    login.error_1.setText("Contraseña cambiada con éxito")
-                    time.sleep(1)
-                    login.close()
-                    app.quit()
-                    # login.stackedWidget.setCurrentIndex(2)
-                    # login.log_in_2.clicked.connect(lambda: Login.show_page_3(row))
-    
-    # def show_page_3(row):
-#login.log_in.clicked.connect(Login.gui_login)
-main.show()
-#login.show()
-app.exec()
-    # def gui_changePassword(self):
-    #     login.changePassword.clicked.connect(self.gui_changePassword)
-    # # Ejecutable
+    def __init__(self):
+        self.login_controller = LoginController(login)
+        login.log_in.clicked.connect(self.login_controller.gui_login)
+        login.log_in_1.clicked.connect(lambda: self.login_controller.show_page_2(list()))
+        login.show()
+        app.exec()
+         
+class Main():
+
+    def show_page_3(row): 
+        main.show()
+
+
+
