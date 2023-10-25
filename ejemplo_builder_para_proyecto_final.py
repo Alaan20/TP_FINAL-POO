559ae3a015200f3df2310763ccdc798ee32c2eb4
Alan Naim <alanviera.1881@gmail.com>
2023-10-24 23:33:15 -0300

    Reduccion de contenidos del service
    Se eliminaron algunos elementos de cada service con el fin de brindar una interfaz mas reducida y mejor a simple vista.

--- ejemplo_builder_para_proyecto_final.py
+++ ejemplo_builder_para_proyecto_final.py
@@ -13,7 +13,7 @@ class builderService(ABC):
     def servicios_basico(self)->None:
         pass
     @abstractmethod
-    def servicios_standar(self)->None:
+    def servicios_estandar(self)->None:
         pass
     @abstractmethod
     def servicios_completo(self)->None:
@@ -31,53 +31,37 @@ class concretebuilderService(builderService):
         return service
     
     def servicios_basico(self) -> None:
-        self._service.agregar("Elementos de seguridad: revision y resposicion de luces exteriores y baul")
-        self._service.agregar("Elementos de seguridad: revision de presion de neuomaticos(TI)")
-        self._service.agregar("Elementos de seguridad: revision de presion de neuomaticos(TD)")
-        self._service.agregar("Elementos de seguridad: revision de presion de neuomaticos(DI)")
-        self._service.agregar("Elementos de seguridad: revision de presion de neuomaticos(DD)")
-        self._service.agregar("Fluidos del vehiculo: bateria")
-        self._service.agregar("Lubricantes y filtros: revision filtro de combustible")
-        self._service.agregar("Lubricantes y filtros: revision aceite diferencial")
-        self._service.agregar("Lubricantes y filtros: cambio de aceite y filtro")
+        self._service.agregar("Elementos de Seguridad: Revision y reposicion de luces exteriores y baul")
+        self._service.agregar("Elementos de Seguridad: Amortiguadores")
+        self._service.agregar("Elementos de Seguridad: Revision general de presion de neumaticos")
+        self._service.agregar("Elementos de Seguridad: Revision tuerca neumaticos")
+        
+        self._service.agregar("Fluidos del Vehiculo: Bateria")
+        
+        self._service.agregar("Lubricantes y Filtros: Revision filtro de combustible")
+        self._service.agregar("Lubricantes y Filtros: Revision aceite diferencial")
+        self._service.agregar("Lubricantes y Filtros: Cambio de aceite y filtro")
         self._service.ordenar()
     
-    def servicios_standar(self) -> None:
-        self._service.agregar("Elementos de seguridad: revision tuerca neumaticos")
-        self._service.agregar("Elementos de seguridad: revisar/reemplazar escobillas limpiaparabrisas")
-        self._service.agregar("Elementos de seguridad: pastillas de freno")
-        self._service.agregar("Elementos de seguridad: disco de freno")
-        self._service.agregar("Elementos de seguridad: amortiguadores")
-        self._service.agregar("Fluidos del vehiculo: liquido de direccion hidraulica")
-        self._service.agregar("Fluidos del vehiculo: liquido limpiaparabrisas")
-        self._service.agregar("Fluidos del vehiculo: revision liquido de frenos")
-        self._service.agregar("Lubricantes y filtros: revision aceite caja de transferencia")
-        self._service.agregar("Lubricantes y filtros: reemplazo de filtro de aire")
-        self._service.agregar("Partes mecanicas: bisagras de puertas y engrase retenedor de puertas")
-        self._service.agregar("Partes mecanicas: caño de escape")
-        self._service.agregar("Partes mecanicas: correa alternador")
-        self._service.agregar("Partes mecanicas: correa direccion asistida")
-        self._service.agregar("ABS: airbag")
-        self._service.agregar("ABS: inyeccion")
-        self._service.agregar("ABS: sensores y actuadores")
+    def servicios_estandar(self) -> None:
+        self._service.agregar("Fluidos del Vehiculo: Liquido de direccion hidraulica y del limpiaparabrisas")
+        self._service.agregar("Fluidos del Vehiculo: Revision liquido de frenos")
+        
+        self._service.agregar("Partes Mecanicas: Bisagras de puertas y engrase retenedor de puertas")
+        self._service.agregar("Partes Mecanicas: Caño de escape")
+        self._service.agregar("Partes Mecanicas: Correa alternador y de Direccion Asistida")
+        
+        self._service.agregar("ABS: Airbag")
+        self._service.agregar("ABS: Inyeccion")
+        self._service.agregar("ABS: Sensores y Actuadores")
         self._service.ordenar()
     
     def servicios_completo(self) -> None:
-        self._service.agregar("Elementos de seguridad: flexibles de freno")
-        self._service.agregar("Elementos de seguridad: ajuste de presion de los neumaticos T")
-        self._service.agregar("Elementos de seguridad: ajuste de presion de los neumaticos D")
-        self._service.agregar("Fluidos del vehiculo: revision liquido refrigerante/anticongelante")
-        self._service.agregar("Fluidos del vehiculo: punto de congelamiento")
-        self._service.agregar("Lubricantes y filtros: revision aceite caja de cambios")
-        self._service.agregar("Partes mecanicas: arandela tapon de carter")
-        self._service.agregar("Partes mecanicas: correa aire condicionado")
-        self._service.agregar("Partes mecanicas: guardapolvos y holguras de transmicion")
-        self._service.agregar("Partes mecanicas: revision de mangueras")
-        self._service.agregar("Pruebas dinamicas: cinturon de seguridad(comprobar funcionamiento inercial)")
-        self._service.agregar("ABS: climatizacion")
-        self._service.agregar("ABS: historial de fallas")
-        self._service.agregar("ABS: instrumental")
-        self._service.agregar("Escaneo con computadora: diagnostico y reparacion de errores")
+        self._service.agregar("Pruebas Dinamicas: Cinturon de Seguridad (Comprobar Funcionamiento Inercial)")
+        self._service.agregar("ABS: Climatizacion")
+        self._service.agregar("ABS: Historial de fallas")
+        self._service.agregar("ABS: Instrumental")
+        self._service.agregar("Escaneo con computadora: Diagnostico y reparacion de errores")
         self._service.ordenar()
 
 class Service():
@@ -88,12 +72,13 @@ class Service():
         self._parts.append(servicios)
     
     def listado_De_servicios(self):
-        print(f"servivios a aplicar:\n")
+        print(f"Servicios a aplicar:\n")
         for i in self._parts:
             print(i,end='\n')
     
     def ordenar(self):
         self._parts.sort()
+
 class Director():
     def __init__(self) -> None:
         self._builder=None
@@ -106,24 +91,14 @@ class Director():
         self._builder=builder
     def service_basico(self):
         self.builder.servicios_basico()
-    def service_standar(self):
+    def service_estandar(self):
         self.builder.servicios_basico()
-        self.builder.servicios_standar()
+        self.builder.servicios_estandar()
     def service_completo(self):
         self.builder.servicios_basico()
-        self.builder.servicios_standar()
+        self.builder.servicios_estandar()
         self.builder.servicios_completo()
 
 director=Director()
 builder=concretebuilderService()
 director.builder=builder
-
-# print("service basico: \n")
-# director.service_basico()
-# builder.service.listado_De_servicios()
-# print("service standar:\n")
-# director.service_standar()
-# builder.service.listado_De_servicios()
-# print("service completo: \n")
-# director.service_completo()
-# builder.service.listado_De_servicios()
