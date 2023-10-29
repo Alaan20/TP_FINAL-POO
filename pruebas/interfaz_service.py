85db35e4202f362869780e4116c77978a4e4ef75
Alan Naim <alanviera.1881@gmail.com>
2023-10-28 23:50:28 -0300

    Up_InterfazService
    En esta actualización, se puede cambiar el tipo de Service y el Layout se adapta al tipo elegido sin sobreponerse con los demás.

--- pruebas/interfaz_service.py
+++ pruebas/interfaz_service.py
@@ -3,78 +3,111 @@ from PyQt5.QtCore import *
 import sys
 from ejemplo_builder_para_proyecto_final import Director, concretebuilderService
 
-
-class Window (QMainWindow):
+class VentanaPrincipal (QWidget):
 
     def __init__(self):
-        super(QMainWindow,self).__init__()
-        self.layout1 = QVBoxLayout()
+        super().__init__()
+        self.__init_ui()
+    
+    def __init_ui(self):
+        layout = QVBoxLayout()
+        self.__combo_box = QComboBox()
+        self.__combo_box.addItems(["Basico","Estandar","Completo"])
+        layout.addWidget(self.__combo_box)
         
-        btn1 = QRadioButton('Basico')
-        btn1.pressed.connect(self.service_basico)
+        self.__combo_box.currentTextChanged.connect(self.__actualizar_ventana)
         
-        btn2 = QRadioButton('Estandar')
-        btn2.pressed.connect(self.service_estandar)
-        
-        btn3 = QRadioButton('Completo')
-        btn3.pressed.connect(self.service_completo)
-        
-        self.layout1.addWidget(btn1)
-        self.layout1.addWidget(btn2)
-        self.layout1.addWidget(btn3)
-        
-        widget1 = QWidget()
-        widget1.setLayout(self.layout1)
-        self.setCentralWidget(widget1)
+        self.__ventana_basica = VentanaService("Basico")
+        self.__ventana_estandar = VentanaService("Estandar")
+        self.__ventana_completa = VentanaService("Completo")
 
-    def service_basico (self):
-        widget_completo = QWidget()
+        layout.addWidget(self.__ventana_basica)
+        layout.addWidget(self.__ventana_estandar)
+        layout.addWidget(self.__ventana_completa)
+        
+        self.setLayout(layout)
+        self.__reinicio_ventana()
+        self.__actualizar_ventana()
+
+    def __actualizar_ventana (self):
+        opcion = self.__combo_box.currentText()
+        if opcion == "Basico":
+            self.__mostrar_ventana(self.__ventana_basica)
+        elif opcion == "Estandar":
+            self.__mostrar_ventana(self.__ventana_estandar)
+        elif opcion == "Completo":
+            self.__mostrar_ventana(self.__ventana_completa)
+
+    def __reinicio_ventana (self):
+        for v in [self.__ventana_basica, self.__ventana_estandar,self.__ventana_completa]:
+            v.hide()
+    
+    def __mostrar_ventana (self, ventana):
+        self.__reinicio_ventana()
+        ventana.show()
+
+
+class VentanaService(QWidget):
+    
+    def __init__(self, nombre):
+        super().__init__()
+        self.__busco_ui(nombre)
+
+    def __busco_ui (self, nombre):
+        if nombre == "Basico":
+            self.__init_ui_basico()
+        elif nombre == "Estandar":
+            self.__init_ui_estandar()
+        elif nombre == "Completo":
+            self.__init_ui_completo()
+
+    def __init_ui_basico (self):
+        layout = QVBoxLayout()
         direc = Director()
         direc.builder = concretebuilderService() 
         direc.service_basico()
         
-        self.asigno(direc.builder.service._parts,widget_completo)
-        self.setCentralWidget(widget_completo)
+        layout = asigno_layout(direc.builder.service._parts)
+        self.setLayout(layout)
 
-    def service_estandar (self):
-        widget_completo = QWidget()
+    def __init_ui_estandar (self):
+        layout = QVBoxLayout()
         direc = Director()
         direc.builder = concretebuilderService()
-        direc.service_standar()
+        direc.service_estandar()
         
-        self.asigno(direc.builder.service._parts, widget_completo)
-        self.setCentralWidget(widget_completo)
+        layout = asigno_layout(direc.builder.service._parts)
+        self.setLayout(layout)
 
-
-    def service_completo (self):
-        widget_terminado = QWidget()
+    def __init_ui_completo (self):
         direc = Director()
         direc.builder = concretebuilderService()
         direc.service_completo()
         
-        self.asigno(direc.builder.service._parts, widget_terminado)
-        self.setCentralWidget(widget_terminado)
+        layout = asigno_layout(direc.builder.service._parts)
+        self.setLayout(layout)
 
-    def asigno (self, lista, widget):
-        layout1 = QVBoxLayout()
-        layout2 = QVBoxLayout()
-        layout_completo = QHBoxLayout()
-        
-        for i in lista:
-            lbl = QLabel(i)
-            layout1.addWidget(lbl)
-            combo = QComboBox()
-            combo.addItem('Excelente')
-            combo.addItem('Intermedio')
-            combo.addItem('Defectuoso')
-            layout2.addWidget(combo)
+def asigno_layout (lista):
+    layout1 = QVBoxLayout()
+    layout2 = QVBoxLayout()
+    layout_completo = QHBoxLayout()
+    
+    for i in lista:
+        lbl = QLabel(i)
+        layout1.addWidget(lbl)
+        combo = QComboBox()
+        combo.addItem('Excelente')
+        combo.addItem('Intermedio')
+        combo.addItem('Defectuoso')
+        layout2.addWidget(combo)
 
-        layout_completo.addLayout(layout1)
-        layout_completo.addLayout(layout2)
-        widget.setLayout(layout_completo)
-        return widget
+    layout_completo.addLayout(layout1)
+    layout_completo.addLayout(layout2)
+    
+    return layout_completo
 
-app = QApplication(sys.argv)
-window = Window()
-window.show()
-sys.exit(app.exec())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ventana_principal = VentanaPrincipal()
+    ventana_principal.show()
+    sys.exit(app.exec())
