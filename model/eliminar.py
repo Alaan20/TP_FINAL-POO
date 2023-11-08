eb70a68079ebbc4fe1b8b8fa39ed686da826d801
dukireal <130255243+dukireal@users.noreply.github.com>
2023-11-08 14:05:47 -0300

    aliminar actualizado a baja logica

--- model/eliminar.py
+++ model/eliminar.py
@@ -1,17 +1,18 @@
 from database.database import PersonaDb
 
-class EliminarController:
-    def __init__(self,main):
-        self._main = main
-        self._db = PersonaDb()
-        self._usuario = None
+class Eliminar():
+    def __init__(self,main) -> None:
+        self._main=main
+        self._bd=PersonaDb()
+        self._id=None
+        self._select_row=[]
+        
+    def eliminar_usuarios(self):
+        self._id= self._main.table_user.item(self._main.table_user.currentRow(),0)
+        self._bd.borrado(self._id.text())
+    def elimininar_mecanico(self):
+        self._id=self._main.Tableuser_2.item(self._main.Tableuser_2.currentRow(),0)
+        self._bd.borrado(self._id.text())
     
-    def eliminar_usuario(self):    
-        self._usuario = self._main.table_user.item(self._main.table_user.currentRow(),1)
-        self._db.borrado(self._usuario.text())
-        self._main.table_user.removeRow(self._main.table_user.currentRow())
-    
-    def eliminar_mecanico(self):    
-        self._usuario = self._main.Tableuser_2.item(self._main.Tableuser_2.currentRow(),1)
-        self._db.borrado(self._usuario.text())
-        self._main.Tableuser_2.removeRow(self._main.Tableuser_2.currentRow())
+    def eliminar_autos(self):
+        pass
